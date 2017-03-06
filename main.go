8b005f3e71593877396fc4e187e672fd786e3dad
Phu Nguyen <ndphu@localhost.com>
2017-03-06 17:48:15 +0700

    listen from the Mosquito mqtt

--- main.go
+++ main.go
@@ -2,34 +2,40 @@ package main
 
 import (
 	"github.com/ndphu/espresso-commons"
-	"github.com/ndphu/espresso-commons/db"
+	"github.com/ndphu/espresso-commons/dao"
+	"github.com/ndphu/espresso-commons/messaging"
 	"github.com/ndphu/espresso-commons/model"
-	"github.com/ndphu/manga-crawler/dao"
+	"github.com/ndphu/espresso-commons/repo"
 	"golang.org/x/net/websocket"
 	"gopkg.in/gin-gonic/gin.v1"
 	"gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 	"io"
 	"log"
+
+	"fmt"
 	"net/http"
 )
 
 var (
-	Session  *mgo.Session
-	Database *mgo.Database
+	Session        *mgo.Session
+	Database       *mgo.Database
+	MessageRounter *messaging.MessageRouter
 )
 
 func EchoServer(ws *websocket.Conn) {
-	var msg []byte
-	_, err := ws.Read(&msg)
-	if err != nil {
-		log.Println(err)
-	}
-	log.Println("Client:", msg)
 	io.Copy(ws, ws)
 }
 
+type IRAgentMessageHandler struct {
+}
+
+func (i *IRAgentMessageHandler) OnNewMessage(msg *model.Message) {
+	log.Println("New message:", msg.Type)
+}
+
 func main() {
+	// database
 	log.Println("Connecting to database")
 	s, err := mgo.Dial("127.0.0.1:27017")
 	if err != nil {
@@ -40,20 +46,41 @@ func main() {
 	Session = s
 	Database = Session.DB(commons.DBName)
 
-	IRMessageRepo := &db.IREventRepo{
+	IRMessageRepo := &repo.IREventRepo{
 		Database: Database,
 		Session:  Session,
 	}
+	// end database
 
+	// messaging
+	log.Println("Connecting to broker...")
+	MessageRounter, err = messaging.NewMessageRouter("127.0.0.1", 1883, "", "", fmt.Sprintf("espresso-ui-%d", commons.GetRandom()))
+	if err != nil {
+		panic(err)
+	}
+
+	irAgentMessageHandler := IRAgentMessageHandler{}
+
+	err = MessageRounter.Subscribe(commons.IRAgentEventTopic, &irAgentMessageHandler)
+	if err != nil {
+		panic(err)
+	}
+	defer MessageRounter.Unsubscribe(commons.IRAgentEventTopic, &irAgentMessageHandler)
+
+	// end messaging
+
+	// web backend
 	r := gin.Default()
 
 	r.GET("/esp/v1/event/ir", func(c *gin.Context) {
 		irMessages := make([]model.IRMessage, 0)
-		//err := dao.GetAll(IRMessageRepo, bson.M{}, &irMessage)
-		err := dao.GetAllWithRangeAndSort(IRMessageRepo, bson.M{}, 9999, 0, "-timestamp", &irMessages)
+		err := dao.FindAllWithSort(IRMessageRepo, bson.M{}, 0, 100, "-_id", &irMessages)
 		if err != nil {
 			c.JSON(http.StatusInternalServerError, gin.H{"err": err.Error()})
 		} else {
+			for i := 0; i < len(irMessages); i++ {
+				irMessages[i].UnixTimestamp = irMessages[i].Timestamp.Unix()
+			}
 			c.JSON(http.StatusOK, irMessages)
 		}
 	})
@@ -64,4 +91,5 @@ func main() {
 	})
 
 	r.Run(":80")
+	// end web backend
 }
--- ui/modules/Events.js
+++ ui/modules/Events.js
@@ -22,14 +22,57 @@ class Events extends Component {
         })
     }
 
+    timeDifference(current, previous) {
+
+        var msPerMinute = 60 * 1000;
+        var msPerHour = msPerMinute * 60;
+        var msPerDay = msPerHour * 24;
+        var msPerMonth = msPerDay * 30;
+        var msPerYear = msPerDay * 365;
+
+        var elapsed = current - previous;
+
+        if (elapsed < msPerMinute) {
+            const secCount = Math.round(elapsed/1000)
+            if (secCount == 0) {
+                return "Just now"
+            } else if (secCount == 1) {
+                return '1 second ago';
+            } else {
+                return secCount + ' seconds ago';
+            }
+        }
+
+        else if (elapsed < msPerHour) {
+            return Math.round(elapsed/msPerMinute) + ' minutes ago';   
+        }
+
+        else if (elapsed < msPerDay ) {
+            return Math.round(elapsed/msPerHour ) + ' hours ago';   
+        }
+
+        else if (elapsed < msPerMonth) {
+            return 'approximately ' + Math.round(elapsed/msPerDay) + ' days ago';   
+        }
+
+        else if (elapsed < msPerYear) {
+            return 'approximately ' + Math.round(elapsed/msPerMonth) + ' months ago';   
+        }
+
+        else {
+            return 'approximately ' + Math.round(elapsed/msPerYear ) + ' years ago';   
+        }
+    }
+
     render() {
+        var now = new Date().getTime()
         var eventRows = this.state.events.map(e => 
             <TableRow key={"key-event-row-id-" + e.id} >
                 <TableRowColumn>{e.remoteName}</TableRowColumn>
                 <TableRowColumn><b>{e.button}</b></TableRowColumn>
                 <TableRowColumn>{e.repeat}</TableRowColumn>
                 <TableRowColumn>{e.source}</TableRowColumn>
-                <TableRowColumn>{e.timestamp}</TableRowColumn>
+                <TableRowColumn>{this.timeDifference(now, e.unixTimestamp * 1000)}</TableRowColumn>
             </TableRow>
         )
 
